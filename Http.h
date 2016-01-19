367737c088c76e04df2111d7bc008b5ba534fa8a
steve <saint@aliyun.com>
2016-01-19 18:47:42 +0800

    remove macros

--- Http.h
+++ Http.h
@@ -3,15 +3,12 @@
 
 #include <sys/socket.h>
 
-#define SAFE_DELETE(p)           do { delete (p); (p) = nullptr; } while(0)
-#define SAFE_DELETE_ARRAY(p)     do { if(p) { delete[] (p); (p) = nullptr; } } while(0)
-#define SAFE_FREE(p)             do { if(p) { free(p); (p) = nullptr; } } while(0)
-#define SAFE_RELEASE(p)          do { if(p) { (p)->release(); } } while(0)
-#define SAFE_RELEASE_NULL(p)     do { if(p) { (p)->release(); (p) = nullptr; } } while(0)
-#define SAFE_RETAIN(p)           do { if(p) { (p)->retain(); } } while(0)
+#define DELETE(p)           do { delete (p); (p) = nullptr; } while(0)
+#define DELETE_ARRAY(p)     do { delete[] (p); (p) = nullptr; } while(0)
+#define FREE(p)             do { free(p); (p) = nullptr;} while(0)
+#define RELEASE(p)          do { (p)->release(); } while(0)
+#define RELEASE_NULL(p)     do { (p)->release(); (p) = nullptr; } while(0)
 #define BREAK_IF(cond)           if(cond) break
-
-
 
 namespace kttp {
 
